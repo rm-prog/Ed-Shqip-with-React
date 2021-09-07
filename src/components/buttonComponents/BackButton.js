d915c7938d21a4397c4ce6bc12240cfcc760c4f8
Rminollari <rminollari@gmail.com>
2021-09-07 20:10:51 +0200

    deleted unnessecary old folders

--- src/components/buttonComponents/BackButton.js
+++
@@ -1,9 +0,0 @@
-import styles from '../../styles/mainPages.module.css'
-
-const BackButton = ( { onClick } ) => {
-    return (
-        <button onClick={onClick} className={styles.backButton}> &lt; Kthehu </button>
-    )
-}
-
-export default BackButton
--- src/components/buttonComponents/StartButton.js
+++
@@ -1,9 +0,0 @@
-import styles from '../../styles/wordGame.module.css'
-
-const StartButton = ( { onClick, display = styles.displayBlock, text = 'Filloni Lojen', disabled = false } ) => {
-    return (
-        <button className={`${styles.startButton} ${display}`} onClick={onClick} disabled={disabled}> {text} </button>
-    )
-}
-
-export default StartButton
